2fb187aa2e551a5fe0bfcf1484cc71647ab56d3d
Kryz14 <ctechlanre@gmail.com>
2025-06-04 02:56:26 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import NavBar from "./components/NavBar"
+import Contact from "./sections/Contact"
+import ExperienceSection from "./sections/ExperienceSection"
+import FeatureCards from "./sections/FeatureCards"
+import Footer from "./sections/Footer"
+import Hero from "./sections/Hero"
+import LogoShowcase from "./sections/LogoShowcase"
+import ShowcaseSection from "./sections/ShowcaseSection"
+import TechStack from "./sections/TechStack"
+import Testimonials from "./sections/Testimonials"
+
+
+const App = () => {
+  return (
+    <>
+      <NavBar />
+      <Hero />
+      <ShowcaseSection />
+      <LogoShowcase />
+      <FeatureCards />
+      <ExperienceSection />
+      <TechStack />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/constants/index.js
@@ -0,0 +1,247 @@
+const navLinks = [
+    {
+      name: "Work",
+      link: "#work",
+    },
+    {
+      name: "Experience",
+      link: "#experience",
+    },
+    {
+      name: "Skills",
+      link: "#skills",
+    },
+    {
+      name: "Testimonials",
+      link: "#testimonials",
+    },
+  ];
+  
+  const words = [
+    { text: "Ideas", imgPath: "/images/ideas.svg" },
+    { text: "Concepts", imgPath: "/images/concepts.svg" },
+    { text: "Designs", imgPath: "/images/designs.svg" },
+    { text: "Code", imgPath: "/images/code.svg" },
+    { text: "Ideas", imgPath: "/images/ideas.svg" },
+    { text: "Concepts", imgPath: "/images/concepts.svg" },
+    { text: "Designs", imgPath: "/images/designs.svg" },
+    { text: "Code", imgPath: "/images/code.svg" },
+  ];
+  
+  const counterItems = [
+    { value: 5, suffix: "+", label: "Years of Experience" },
+    { value: 30, suffix: "+", label: "Satisfied Clients" },
+    { value: 20, suffix: "+", label: "Completed Projects" },
+    { value: 90, suffix: "%", label: "Client Retention Rate" },
+  ];
+  
+  const logoIconsList = [
+    {
+      imgPath: "/images/logos/company-logo-1.png",
+    },
+    {
+      imgPath: "/images/logos/company-logo-2.png",
+    },
+    {
+      imgPath: "/images/logos/company-logo-3.png",
+    },
+    {
+      imgPath: "/images/logos/company-logo-4.png",
+    },
+    {
+      imgPath: "/images/logos/company-logo-5.png",
+    },
+    {
+      imgPath: "/images/logos/company-logo-6.png",
+    },
+    {
+      imgPath: "/images/logos/company-logo-7.png",
+    },
+    {
+      imgPath: "/images/logos/company-logo-8.png",
+    },
+    {
+      imgPath: "/images/logos/company-logo-9.png",
+    },
+    {
+      imgPath: "/images/logos/company-logo-10.png",
+    },
+    {
+      imgPath: "/images/logos/company-logo-11.png",
+    },
+  ];
+  
+  const abilities = [
+    {
+      imgPath: "/images/seo.png",
+      title: "Quality Focus",
+      desc: "Delivering high-quality results while maintaining attention to every detail.",
+    },
+    {
+      imgPath: "/images/chat.png",
+      title: "Reliable Communication",
+      desc: "Keeping you updated at every step to ensure transparency and clarity.",
+    },
+    {
+      imgPath: "/images/time.png",
+      title: "On-Time Delivery",
+      desc: "Making sure projects are completed on schedule, with quality & attention to detail.",
+    },
+  ];
+  
+  const techStackImgs = [
+    {
+      name: "React Developer",
+      imgPath: "/images/logos/react.png",
+    },
+    {
+      name: "Python Developer",
+      imgPath: "/images/logos/python.svg",
+    },
+    {
+      name: "Backend Developer",
+      imgPath: "/images/logos/node.png",
+    },
+    {
+      name: "Interactive Developer",
+      imgPath: "/images/logos/three.png",
+    },
+    {
+      name: "Project Manager",
+      imgPath: "/images/logos/git.svg",
+    },
+  ];
+  
+  const techStackIcons = [
+    {
+      name: "React Developer",
+      modelPath: "/models/react_logo-transformed.glb",
+      scale: 1,
+      rotation: [0, 0, 0],
+    },
+    {
+      name: "Python Developer",
+      modelPath: "/models/python-transformed.glb",
+      scale: 0.8,
+      rotation: [0, 0, 0],
+    },
+    {
+      name: "Backend Developer",
+      modelPath: "/models/node-transformed.glb",
+      scale: 5,
+      rotation: [0, -Math.PI / 2, 0],
+    },
+    {
+      name: "Interactive Developer",
+      modelPath: "/models/three.js-transformed.glb",
+      scale: 0.05,
+      rotation: [0, 0, 0],
+    },
+    {
+      name: "Project Manager",
+      modelPath: "/models/git-svg-transformed.glb",
+      scale: 0.05,
+      rotation: [0, -Math.PI / 4, 0],
+    },
+  ];
+  
+  const expCards = [
+    {
+      review: "Chris brought creativity and technical expertise to the team, significantly improving our frontend performance. His work has been invaluable in delivering faster experiences.",
+      imgPath: "/images/exp1.png",
+      logoPath: "/images/logo1.png",
+      title: "Frontend Developer",
+      date: "January 2024 - September 2024",
+      responsibilities: [
+        "Developed and maintained user-facing features for the Shopware website.",
+        "Collaborated closely with UI/UX designers to ensure seamless user experiences.",
+        "Optimized web applications for maximum speed and scalability.",
+      ],
+    },
+    {
+      review: "Chris’s contributions to our Web  applications have been outstanding. He approaches challenges with a problem-solving mindset.",
+      
+      logoPath: "/images/logo2.png",
+      title: "Full Stack Developer",
+      date: "December 2024 - April 2025",
+      responsibilities: [
+        "Led the development of Strictly Messiah web applications, focusing on scalability.",
+        "Worked with backend engineers to integrate APIs seamlessly with the frontend.",
+        
+      ],
+    },
+    
+  ];
+  
+  const expLogos = [
+    {
+      name: "logo1",
+      imgPath: "/images/logo1.png",
+    },
+    {
+      name: "logo2",
+      imgPath: "/images/logo2.png",
+    },
+    {
+      name: "logo3",
+      imgPath: "/images/logo3.png",
+    },
+  ];
+  
+  const testimonials = [
+    
+    {
+      name: "Marvin McKinney",
+      mentions: "@marvinmckinney",
+      review:
+        "Chris was a pleasure to work with. He turned our outdated website into a fresh, intuitive platform that’s both modern and easy to navigate. Fantastic work overall.",
+      
+    },
+    {
+      name: "Floyd Miles",
+      mentions: "@floydmiles",
+      review:
+        "Chris’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional!",
+      
+    },
+    {
+      name: "Damilola Obaseki",
+      mentions: "@damilolaobaseki",
+      review:
+        "Chris was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend and backend dev are top-notch.",
+      
+    },
+  ];
+  
+  const socialImgs = [
+    {
+      name: "insta",
+      imgPath: "/images/insta.png",
+    },
+    {
+      name: "fb",
+      imgPath: "/images/fb.png",
+    },
+    {
+      name: "x",
+      imgPath: "/images/x.png",
+    },
+    {
+      name: "linkedin",
+      imgPath: "/images/linkedin.png",
+    },
+  ];
+  
+  export {
+    words,
+    abilities,
+    logoIconsList,
+    counterItems,
+    expCards,
+    expLogos,
+    testimonials,
+    socialImgs,
+    techStackIcons,
+    techStackImgs,
+    navLinks,
+  };
